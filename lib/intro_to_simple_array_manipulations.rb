5dc32eb2a819079db8e1f0f3441e20820a8fabdf
Alex Farmer <farmale10@gmail.com>
2019-08-28 19:27:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,12 +23,12 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat()
-  
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert()
-  
+def using_insert(array, element)
+  array.
 end
 
 def using_uniq()
